656f5508434909dfbb24a39dbf59909b308bb71d
miguel <miguelcruz201715@gmail.com>
2025-09-02 20:17:31 -0500

    Repasojs

---
+++ 01-HolaMundo/holamundo.js
@@ -0,0 +1,32 @@
+console.log('Hola mundo');
+
+let cajaDeAndy = "woody";
+
+console.log(cajaDeAndy);
+
+let edad = 26;
+let nombre = "miguelcruz"
+
+console.log('mi nombre es', nombre, "y mi edad es", edad)
+
+//Array
+
+let frutas = ['manzana', 'banano', 'mango'];
+
+console.log(frutas)
+
+console.log(frutas[1])
+
+let frase = "JaveScrip es extremadamente genial";
+
+console.log(frase);
+console.log(frase.length);
+mayusculas = frase.toUpperCase();
+console.log(mayusculas);
+minusculas = frase.toLowerCase();
+console.log(minusculas); 
+
+//operaciones matematicas
+const suma = 3 + 12;
+
+console.log(suma);
